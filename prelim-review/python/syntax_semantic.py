8db84b82a7d54c1164a07711af4b9a6502bb2d4b
Student <student@Uphslmaclab26.local>
2025-09-19 18:51:13 +0800

    Add syntax error explanations

---
+++ prelim-review/python/syntax_semantic.py
@@ -0,0 +1,11 @@
+# Syntax errors occur when the written code violates
+# the language's grammatical rules. This causes the code
+# to fail execution, because the compiler/parser needs the right grammar
+# in order to understand how to execute it.
+
+x = 10
+print x;
+
+# If the code above is executed, it will result in an error.
+# A correct print statement in Python written as print().
+
