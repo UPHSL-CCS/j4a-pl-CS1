0c1eb56ca30772a146707ea3e7e9ac5779227a53
Student <student@Uphslmaclab26.local>
2025-09-19 19:00:40 +0800

    Add code and explanation for syntax errors

--- prelim-review/python/syntax_semantic.py
+++ prelim-review/python/syntax_semantic.py
@@ -1,7 +1,7 @@
 # Syntax errors occur when the written code violates
 # the language's grammatical rules. This causes the code
-# to fail execution, because the compiler/parser needs the right grammar
-# in order to understand how to execute it.
+# to fail execution, because the compiler/parser cannot understand 
+# how to do it.
 
 x = 10
 print x;
@@ -9,3 +9,12 @@ print x;
 # If the code above is executed, it will result in an error.
 # A correct print statement in Python written as print().
 
+# Another code snippet with glaring errors is as follows:
+
+print x; # ERROR: Missing parentheses in the keyword.
+
+def myFunction: # ERROR: Missing parentheses in the function name.
+    y x + 1 # ERROR: Missing assignment operator.
+    retur y # ERROR: Misspelled keyword.
+
+2x = 11 # ERROR: Variable name invalid because it starts with a number.
