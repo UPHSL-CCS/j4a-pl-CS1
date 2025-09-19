d3972bb99c93ef1c2bdf216a2e6b9ac49e92c290
Student <student@Uphslmaclab26.local>
2025-09-19 19:04:34 +0800

    Change .py to make separate syntax and semantic files

--- prelim-review/python/syntax_errors.py
+++ prelim-review/python/syntax_errors.py
@@ -9,7 +9,9 @@ print x;
 # If the code above is executed, it will result in an error.
 # A correct print statement in Python written as print().
 
-# Another code snippet with glaring errors is as follows:
+# More code with glaring errors is as follows:
+
+print(y) # ERROR: Cannot use variable before it is declared.
 
 print x; # ERROR: Missing parentheses in the keyword.
 
@@ -18,3 +20,4 @@ def myFunction: # ERROR: Missing parentheses in the function name.
     retur y # ERROR: Misspelled keyword.
 
 2x = 11 # ERROR: Variable name invalid because it starts with a number.
+
