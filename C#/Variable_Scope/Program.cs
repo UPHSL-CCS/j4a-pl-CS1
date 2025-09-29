b1927521d42383899e286ee5af05defd3a7bfd29
Junell E. Torres <torresjunell80@gmail.com>
2025-09-29 22:14:06 +0800

    Added C# folder with sample programs

---
+++ C#/Variable_Scope/Program.cs
@@ -0,0 +1,14 @@
+﻿// VariableScope.cs
+using System;
+
+class VariableScopeExample {
+    // Global variable
+    static int balance = 1000;
+
+    static void Main() {
+        int deposit = 500;      // Local variable
+        balance += deposit;
+
+        Console.WriteLine("Balance after deposit: " + balance);
+    }
+}
